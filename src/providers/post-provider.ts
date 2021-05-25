44a375306e48816b20216e654a805a985372c7a1
Samuel Botwe <botwesamuel192@gmail.com>
2021-05-25 02:43:41 +0000

    home 25%

--- src/providers/post-provider.ts
+++ src/providers/post-provider.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class PostProvider {
     // server : string ="http://www.camfilaknust.com/" //api url
-    server: string = "http://10.152.59.157:8080/camfilaapi/" //api url
+    server: string = "http://10.149.152.59:8080/camfilaapi/" //api url
     // server : string ="http://10.162.90.177/camfilaapi/" //api url
     //server : string = "http://camfilaknust.tk/"
 
